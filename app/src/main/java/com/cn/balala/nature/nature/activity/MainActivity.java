b79178f1b49f08d4533741f73b15012951b71092
Black <guobin1112@yeah.net>
2015-11-12 08:26:59 +0800

    fragment切换

--- app/src/main/java/com/cn/balala/nature/nature/activity/MainActivity.java
+++ app/src/main/java/com/cn/balala/nature/nature/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cn.balala.nature.nature.activity;
 
-import android.content.Context;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,50 +10,24 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import com.bigkoo.convenientbanner.CBPageAdapter;
-import com.bigkoo.convenientbanner.CBViewHolderCreator;
-import com.bigkoo.convenientbanner.ConvenientBanner;
 import com.cn.balala.nature.R;
-import com.cn.balala.nature.nature.adapter.ToolAdapter;
-import com.cn.balala.nature.nature.model.ToolModel;
-import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
-
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.cn.balala.nature.nature.fragment.ConferenceFragment;
+import com.cn.balala.nature.nature.fragment.MainFragment;
+import com.cn.balala.nature.nature.fragment.MessageFragment;
+import com.cn.balala.nature.nature.fragment.TreeHoleFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    LinearLayout llMainView;
-    GridView gvTools;
     RadioGroup rgNavBar;
     RadioButton rbMain;
     RadioButton rbConference;
     RadioButton rbTreeHole;
     RadioButton rbMessage;
-
-    ConvenientBanner convenientBanner;//顶部广告栏控件
-    ArrayList<Integer> localImages = new ArrayList<Integer>();
-
-    private List<String> networkImages;
-    private String[] images = {"http://p3.so.qhimg.com/t014e7febcac8f81043.jpg",
-            "http://p0.so.qhimg.com/t01b5cfc7ba818053d0.jpg",
-            "http://p0.so.qhimg.com/t01b059c1789b4175d0.jpg"
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,7 +46,6 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         initView();
-        init();
     }
 
     @Override
@@ -110,175 +84,46 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void initView() {
-        llMainView = (LinearLayout) findViewById(R.id.ll_main_view);
-        gvTools = (GridView) llMainView.findViewById(R.id.gv_tools);
         rgNavBar = (RadioGroup) findViewById(R.id.rg_nav_bar);
         rbMain = (RadioButton) findViewById(R.id.rb_main);
         rbConference = (RadioButton) findViewById(R.id.rb_conference);
         rbTreeHole = (RadioButton) findViewById(R.id.rb_tree_hole);
         rbMessage = (RadioButton) findViewById(R.id.rb_message);
 
-        List<ToolModel> list = new ArrayList<>();
-        list.add(new ToolModel(R.drawable.ic_task, "会议"));
-        list.add(new ToolModel(R.drawable.ic_report, "工作报告"));
-        list.add(new ToolModel(R.drawable.ic_news, "新闻动态"));
-        list.add(new ToolModel(R.drawable.ic_leave, "请假"));
-        list.add(new ToolModel(R.drawable.ic_expense_account, "报销"));
-        list.add(new ToolModel(R.drawable.ic_check, "审批"));
-        list.add(new ToolModel(R.drawable.ic_tel_book, "通讯录"));
-        list.add(new ToolModel(R.drawable.ic_note, "备忘录"));
-        list.add(new ToolModel(R.drawable.ic_more, "更多"));
-
-        ToolAdapter adapter = new ToolAdapter(this, list);
-        gvTools.setAdapter(adapter);
-
-        convenientBanner = (ConvenientBanner) findViewById(R.id.banner);
         rbMain.setChecked(true);
 
         rgNavBar.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+
                 switch (i) {
                     case R.id.rb_main:
                         Toast.makeText(MainActivity.this, "首页", Toast.LENGTH_SHORT).show();
+                        MainFragment mainFragment = new MainFragment();
+                        transaction.replace(R.id.layout_fragment, mainFragment);
                         break;
                     case R.id.rb_conference:
                         Toast.makeText(MainActivity.this, "会议", Toast.LENGTH_SHORT).show();
+                        ConferenceFragment conferenceFragment = new ConferenceFragment();
+                        transaction.replace(R.id.layout_fragment, conferenceFragment);
                         break;
                     case R.id.rb_tree_hole:
                         Toast.makeText(MainActivity.this, "树洞", Toast.LENGTH_SHORT).show();
+                        TreeHoleFragment treeHoleFragment = new TreeHoleFragment();
+                        transaction.replace(R.id.layout_fragment, treeHoleFragment);
                         break;
                     case R.id.rb_message:
                         Toast.makeText(MainActivity.this, "消息", Toast.LENGTH_SHORT).show();
+                        MessageFragment messageFragment = new MessageFragment();
+                        transaction.replace(R.id.layout_fragment, messageFragment);
                         break;
-
                 }
+                transaction.commit();
             }
         });
 
-
-    }
-
-    private void init() {
-        initImageLoader();
-//        loadTestDatas();
-        //本地图片例子
-//        convenientBanner.setPages(
-//                new CBViewHolderCreator<LocalImageHolderView>() {
-//                    @Override
-//                    public LocalImageHolderView createHolder() {
-//                        return new LocalImageHolderView();
-//                    }
-//                }, localImages)
-//                //设置两个点图片作为翻页指示器，不设置则没有指示器，可以根据自己需求自行配合自己的指示器,不需要圆点指示器可用不设
-//                .setPageIndicator(new int[]{R.drawable.ic_banner_dot_choose, R.drawable.ic_banner_dot_not_choose});
-
-        //网络加载例子
-        networkImages = Arrays.asList(images);
-        convenientBanner.setPages(new CBViewHolderCreator<NetworkImageHolderView>() {
-            @Override
-            public NetworkImageHolderView createHolder() {
-                return new NetworkImageHolderView();
-            }
-        }, networkImages)
-                //设置两个点图片作为翻页指示器，不设置则没有指示器，可以根据自己需求自行配合自己的指示器,不需要圆点指示器可用不设
-                .setPageIndicator(new int[]{R.drawable.ic_banner_dot_choose, R.drawable.ic_banner_dot_not_choose});
-    }
-
-    //初始化网络图片缓存库
-    private void initImageLoader() {
-        //网络图片例子,结合常用的图片缓存库UIL,你可以根据自己需求自己换其他网络图片库
-        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
-                .cacheInMemory(true).cacheOnDisk(true).build();
-
-        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
-                getApplicationContext()).defaultDisplayImageOptions(defaultOptions)
-                .threadPriority(Thread.NORM_PRIORITY - 2)
-                .denyCacheImageMultipleSizesInMemory()
-                .diskCacheFileNameGenerator(new Md5FileNameGenerator())
-                .tasksProcessingOrder(QueueProcessingType.LIFO).build();
-        ImageLoader.getInstance().init(config);
-    }
-
-    /**
-     * 通过文件名获取资源id 例子：getResId("icon", R.drawable.class);
-     *
-     * @param variableName
-     * @param c
-     * @return
-     */
-    public static int getResId(String variableName, Class<?> c) {
-        try {
-            Field idField = c.getDeclaredField(variableName);
-            return idField.getInt(idField);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return -1;
-        }
-    }
-
-    // 开始自动翻页
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //开始自动翻页
-        convenientBanner.startTurning(5000);
-    }
-
-    // 停止自动翻页
-    @Override
-    protected void onPause() {
-        super.onPause();
-        //停止翻页
-        convenientBanner.stopTurning();
-    }
-
-
-    private class LocalImageHolderView implements CBPageAdapter.Holder<Integer> {
-        private ImageView imageView;
-
-        @Override
-        public View createView(Context context) {
-            imageView = new ImageView(context);
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-            return imageView;
-        }
-
-        @Override
-        public void UpdateUI(Context context, final int position, Integer data) {
-            imageView.setImageResource(data);
-            imageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    //点击事件
-                    Toast.makeText(view.getContext(), "点击了第" + (position + 1) + "图片", Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-    }
-
-    private class NetworkImageHolderView implements CBPageAdapter.Holder<String> {
-        private ImageView imageView;
-
-        @Override
-        public View createView(Context context) {
-            //你可以通过layout文件来创建，也可以像我一样用代码创建，不一定是Image，任何控件都可以进行翻页
-            imageView = new ImageView(context);
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-            return imageView;
-        }
-
-        @Override
-        public void UpdateUI(Context context, final int position, String data) {
-            imageView.setImageResource(R.drawable.ic_leave);//TODO default pic
-            ImageLoader.getInstance().displayImage(data, imageView);
-            imageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    //点击事件
-                    Toast.makeText(view.getContext(), "点击了第" + position + "个", Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
     }
 }
--- app/src/main/java/com/cn/balala/nature/nature/fragment/MainFragment.java
+++ app/src/main/java/com/cn/balala/nature/nature/fragment/MainFragment.java
@@ -1,18 +1,48 @@
 package com.cn.balala.nature.nature.fragment;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bigkoo.convenientbanner.CBPageAdapter;
+import com.bigkoo.convenientbanner.CBViewHolderCreator;
+import com.bigkoo.convenientbanner.ConvenientBanner;
 import com.cn.balala.nature.R;
+import com.cn.balala.nature.nature.adapter.ToolAdapter;
+import com.cn.balala.nature.nature.model.ToolModel;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MainFragment extends Fragment {
+
+    GridView gvTools;
+
+    ConvenientBanner convenientBanner;//顶部广告栏控件
+    ArrayList<Integer> localImages = new ArrayList<Integer>();
+
+    private List<String> networkImages;
+    private String[] images = {"http://p3.so.qhimg.com/t014e7febcac8f81043.jpg",
+            "http://p0.so.qhimg.com/t01b5cfc7ba818053d0.jpg",
+            "http://p0.so.qhimg.com/t01b059c1789b4175d0.jpg"
+    };
 
 
     public MainFragment() {
@@ -24,8 +54,153 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        initView(view);
+        init();
+        return view;
+    }
+
+    private void initView(View view) {
+        gvTools = (GridView) view.findViewById(R.id.gv_tools);
+
+
+        List<ToolModel> list = new ArrayList<>();
+        list.add(new ToolModel(R.drawable.ic_task, "会议"));
+        list.add(new ToolModel(R.drawable.ic_report, "工作报告"));
+        list.add(new ToolModel(R.drawable.ic_news, "新闻动态"));
+        list.add(new ToolModel(R.drawable.ic_leave, "请假"));
+        list.add(new ToolModel(R.drawable.ic_expense_account, "报销"));
+        list.add(new ToolModel(R.drawable.ic_check, "审批"));
+        list.add(new ToolModel(R.drawable.ic_tel_book, "通讯录"));
+        list.add(new ToolModel(R.drawable.ic_note, "备忘录"));
+        list.add(new ToolModel(R.drawable.ic_more, "更多"));
+
+        ToolAdapter adapter = new ToolAdapter(getActivity(), list);
+        gvTools.setAdapter(adapter);
+
+        convenientBanner = (ConvenientBanner) view.findViewById(R.id.banner);
+
+
+    }
+
+    private void init() {
+        initImageLoader();
+//        loadTestDatas();
+        //本地图片例子
+//        convenientBanner.setPages(
+//                new CBViewHolderCreator<LocalImageHolderView>() {
+//                    @Override
+//                    public LocalImageHolderView createHolder() {
+//                        return new LocalImageHolderView();
+//                    }
+//                }, localImages)
+//                //设置两个点图片作为翻页指示器，不设置则没有指示器，可以根据自己需求自行配合自己的指示器,不需要圆点指示器可用不设
+//                .setPageIndicator(new int[]{R.drawable.ic_banner_dot_choose, R.drawable.ic_banner_dot_not_choose});
+
+        //网络加载例子
+        networkImages = Arrays.asList(images);
+        convenientBanner.setPages(new CBViewHolderCreator<NetworkImageHolderView>() {
+            @Override
+            public NetworkImageHolderView createHolder() {
+                return new NetworkImageHolderView();
+            }
+        }, networkImages)
+                //设置两个点图片作为翻页指示器，不设置则没有指示器，可以根据自己需求自行配合自己的指示器,不需要圆点指示器可用不设
+                .setPageIndicator(new int[]{R.drawable.ic_banner_dot_not_choose, R.drawable.ic_banner_dot_choose});
+    }
+
+    //初始化网络图片缓存库
+    private void initImageLoader() {
+        //网络图片例子,结合常用的图片缓存库UIL,你可以根据自己需求自己换其他网络图片库
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheInMemory(true).cacheOnDisk(true).build();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+                getActivity().getApplicationContext()).defaultDisplayImageOptions(defaultOptions)
+                .threadPriority(Thread.NORM_PRIORITY - 2)
+                .denyCacheImageMultipleSizesInMemory()
+                .diskCacheFileNameGenerator(new Md5FileNameGenerator())
+                .tasksProcessingOrder(QueueProcessingType.LIFO).build();
+        ImageLoader.getInstance().init(config);
+    }
+
+    /**
+     * 通过文件名获取资源id 例子：getResId("icon", R.drawable.class);
+     *
+     * @param variableName
+     * @param c
+     * @return
+     */
+    public static int getResId(String variableName, Class<?> c) {
+        try {
+            Field idField = c.getDeclaredField(variableName);
+            return idField.getInt(idField);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
     }
 
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        //开始自动翻页
+        convenientBanner.startTurning(5000);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //停止翻页
+        convenientBanner.stopTurning();
+    }
+
+    private class LocalImageHolderView implements CBPageAdapter.Holder<Integer> {
+        private ImageView imageView;
+
+        @Override
+        public View createView(Context context) {
+            imageView = new ImageView(context);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            return imageView;
+        }
+
+        @Override
+        public void UpdateUI(Context context, final int position, Integer data) {
+            imageView.setImageResource(data);
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //点击事件
+                    Toast.makeText(view.getContext(), "点击了第" + (position + 1) + "图片", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private class NetworkImageHolderView implements CBPageAdapter.Holder<String> {
+        private ImageView imageView;
+
+        @Override
+        public View createView(Context context) {
+            //你可以通过layout文件来创建，也可以像我一样用代码创建，不一定是Image，任何控件都可以进行翻页
+            imageView = new ImageView(context);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            return imageView;
+        }
+
+        @Override
+        public void UpdateUI(Context context, final int position, String data) {
+            imageView.setImageResource(R.drawable.ic_leave);//TODO default pic
+            ImageLoader.getInstance().displayImage(data, imageView);
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //点击事件
+                    Toast.makeText(view.getContext(), "点击了第" + position + "个", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
 }
