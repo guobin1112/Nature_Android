a1cd185c33cd641b97ffdb874a8992013a8d25c3
Black <guobin1112@yeah.net>
2015-12-06 11:27:17 +0800

    首页ToolBar布局完善

--- app/src/main/java/com/cn/balala/nature/nature/activity/MainActivity.java
+++ app/src/main/java/com/cn/balala/nature/nature/activity/MainActivity.java
@@ -34,13 +34,17 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
+
         toggle.syncState();
+        toolbar.setNavigationIcon(R.drawable.ic_news);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
